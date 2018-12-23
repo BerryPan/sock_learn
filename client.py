55b057e0952f65440d76714d5e706451a2638adf
BerryPan <313513799@qq.com>
2018-12-23 20:21:07 +0800

    selectors

--- client.py
+++ client.py
@@ -1,8 +1,35 @@
 import socket
+import selectors
+import random
 
-s = socket.socket()
+sel = selectors.DefaultSelector()
 
-s.connect(('localhost', 8080))
-print(s.recv(1024))
-s.send(b'Hello world!')
-s.close()
+
+def write(sock):
+    sock.send(str(random.randint(0, 99)).encode('utf-8'))  # 发送必须是一个byts的数据,需要转码
+    sel.unregister(sock)  # 当发送成功后必须取消注册,用与接下来的接收(读)到数据的注册
+    sel.register(sock, selectors.EVENT_READ, read)  # 注册一个读的事件
+
+
+def read(sock):
+    data = sock.recv(1024)
+    if not data:  # 判断接收到的数据是否为空数据
+        sel.unregister(sock)  # 当接收完成后,依然是取消注册
+        sock.close()  # 到此和服务器的请求基本处理完成,关闭套接字
+        return
+    print('receiver server %s' % data)
+
+
+for i in range(1000):  # 开1000个客户端测试
+    client = socket.socket()
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 防止编辑器的每次运行端口冲突问题
+    client.connect(('localhost', 8080))
+    sel.register(client, selectors.EVENT_WRITE, write)  # 注册一个写的事件,接下来处理发送(写)事件的处理
+
+while True:
+    all_event = sel.select()
+    print(all_event)
+    for event, mask in all_event:
+        sock = event.fileobj
+        callback = event.data  # 创建一个回调函数,方便触发事务执行
+        callback(sock)  # 调用刚创建的回调函数
--- server.py
+++ server.py
@@ -1,35 +1,46 @@
-import selectors
 import socket
-
-sel = selectors.DefaultSelector()
-
-
-def accept(sock, mask):
-    conn, addr = sock.accept()  # Should be ready
-    print('accepted', conn, 'from', addr)
-    conn.setblocking(False)
-    sel.register(conn, selectors.EVENT_READ, read)
+import selectors
 
 
-def read(conn, mask):
-    data = conn.recv(1024)  # Should be ready
-    if data:
-        print('echoing', repr(data), 'to', conn)
-        conn.send(data)
-    else:
-        print('closing', conn)
-        sel.unregister(conn)
-        conn.close()
+class Server(object):
+
+    def __init__(self, sel, sock):
+        self.sel = sel
+        self.sock = sock
+
+    def run(self, host, port):
+        self.sock.bind((host, port))
+        self.sock.listen(50)
+        self.sock.setblocking(False)  # 设置非阻塞
+        self.sel.register(sock, selectors.EVENT_READ, self.accept)
+        while True:
+            events = self.sel.select()  # 默认是阻塞，有活动连接就返回活动连接列表
+            for key, mask in events:
+                callback = key.data  # 创建一个回调函数并获取
+                callback(key.fileobj, mask)  # 调用回调函数
+
+    def accept(self, sock, mask):
+        conn, addr = sock.accept()  # 已经就绪，等待接收
+        print('连接来自于{}'.format(addr))
+        conn.setblocking(False)
+        # sock.send(str('thanks').decode())
+        sel.register(conn, selectors.EVENT_READ, self.read)  # 注册事件
+
+    def read(self, conn, mask):
+        data = conn.recv(1024)  # 就绪，等待接收数据
+        if data:  # 判断是否有数据过来，有就执行
+            print('来自客户端：', data)
+            conn.send(data)
+        else:
+            print('准备关闭连接', conn)
+            sel.unregister(conn)
+            conn.close()
 
 
-sock = socket.socket()
-sock.bind(('localhost', 8080))
-sock.listen()
-sock.setblocking(False)
-sel.register(sock, selectors.EVENT_READ, accept)
-
-while True:
-    events = sel.select()
-    for key, mask in events:
-        callback = key.data
-        callback(key.fileobj, mask)
+if __name__ == '__main__':
+    sel = selectors.DefaultSelector()  # 默认的选择方式
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    host, port = '127.0.0.1', 8080
+    server_obj = Server(sel, sock)
+    server_obj.run(host, port)
